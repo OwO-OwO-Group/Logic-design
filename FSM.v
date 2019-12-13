45a43b069e0e3f622a1385c967a49208cf2a81fc
s5907620 <s0953780241@gmail.com>
2019-12-13 13:21:00 +0800

    update state register

--- FSM.v
+++ FSM.v
@@ -1,11 +1,11 @@
 `timescale 1ns/1ns
-module FSM(clk, coin, drink_choose, change, total_coin);
+module FSM(clk, reset, coin, drink_choose, change, total_coin);
     // coin 投入的零錢, drink_choose 輸入要選擇的飲料
-	// change 找零, total_coin 顯示目前總共投入多少零錢
-	
-    input clk, coin, drink_choose;
+    // change 找零, total_coin 顯示目前總共投入多少零錢
+    
+    input clk, reset, coin, drink_choose;
     output change, total_coin;
-	
+    
     reg total_coin, change;
     reg [1:0] curr_state;
     reg [1:0] next_state;
@@ -14,7 +14,10 @@ module FSM(clk, coin, drink_choose, change, total_coin);
     parameter S1 = 2'b01; // $display drink that you can buy
     parameter S2 = 2'b10; // buy drink
     parameter S3 = 2'b11; // change
-	
-	
-	
+    
+    // state register
+    always@(posedge clk or negedge reset) 
+        if (~reset) curr_state <= S0;         // 1 -> 0 進入state S0
+        else        curr_state <= next_state; 
+    
 endmodule
